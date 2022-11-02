676af9101837b1dae324566dfcc9eec42320240e
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-11-02 14:52:03 +0100

    Add Main

---
+++ src/main/java/de/neuefische/muc222/oop/Main.java
@@ -0,0 +1,16 @@
+package de.neuefische.muc222.oop;
+
+import de.neuefische.muc222.oop.Student;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student emma = new Student("Emma", 12345, "Wuppertal Muster-Strasse 123");
+        System.out.println(emma.getName());
+        System.out.println(emma.getStudentAddress());
+        System.out.println(emma.getStudentId());
+
+
+        System.out.println(emma);
+    }
+}
